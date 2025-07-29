43d40010aec410f8669da02c30d9ff4110722a18
Piotr Fusik <fox@scene.pl>
2025-07-29 13:37:38 +0200

    Music rests.

--- rom2font.pl
+++ rom2font.pl
@@ -2,9 +2,12 @@ open F, '../altirra/ATARIXL.rom' and binmode F or die;
 local $/;
 $_ = <F>;
 for my $i (0 .. 16380) {
-	my $b = ord(substr $_, $i) >> 2;
-	if (ord(substr $_, $i + 1) >> 2 == $b && ord(substr $_, $i + 2) >> 2 == ($b ^ 1) && ord(substr $_, $i + 3) >> 2 == ($b ^ 1)) {
+	my $s = substr($_, $i, 4) & "\xfc" x 4;
+	if (($s ^ substr($s, 0, 1) x 4) eq "\0\0\4\4") {
 		printf "%x\n", $i;
 	}
+#	if ((substr($_, $i, 24) & "\x40" x 24) eq (join '', map $_?"\x40":v0, 0,0,0,0,0,1,1,0,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1)) {
+#		printf "%x\n", $i;
+#	}
 }
 #print length $` if /[\x20-\x23]{2}[\x24-\x27]{2}/;
